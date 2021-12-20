17020dd9f1f8553fff8479fb079ff218b190c43f
Maksym Yankin <yankinmk@gmail.com>
2021-12-20 12:54:58 +0200

    [IMP] base_business_document_import: update partner on bank account if there is no

--- base_business_document_import/models/business_document_import.py
+++ base_business_document_import/models/business_document_import.py
@@ -269,6 +269,7 @@ class BusinessDocumentImport(models.AbstractModel):
         iban = iban.replace(' ', '').upper()
         rpbo = self.env['res.partner.bank']
         rbo = self.env['res.bank']
+        import pdb; pdb.set_trace()
         try:
             validate_iban(iban)
         except:
@@ -285,6 +286,8 @@ class BusinessDocumentImport(models.AbstractModel):
             ('partner_id', '=', partner.id),
             ])
         if bankaccounts:
+            if not bankaccounts[0].partner_id:
+                bankaccounts[0].partner_id = partner
             return bankaccounts[0]
         elif create_if_not_found:
             bank_id = False
