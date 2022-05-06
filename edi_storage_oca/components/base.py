719738efdc701e803f8039d48ff88ccfc583133a
Simone Orsi <simahawk@gmail.com>
2022-05-06 14:16:27 +0200

    edi_storage: path configurable by type and param

--- edi_storage_oca/components/base.py
+++ edi_storage_oca/components/base.py
@@ -1,4 +1,5 @@
 # Copyright 2020 ACSONE
+# Copyright 2022 Camptocamp
 # @author: Simone Orsi <simahawk@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
@@ -44,7 +45,7 @@ class EDIStorageComponentMixin(AbstractComponent):
             (self.backend[direction + "_dir_" + state] or "").strip().rstrip("/")
         )
 
-    def _remote_file_path(self, direction, state, filename):
+    def _remote_file_path(self, direction, state, filename, prefix=None):
         """Return remote file path by direction and state for give filename.
 
         :param direction: string stating direction of the exchange
@@ -52,7 +53,10 @@ class EDIStorageComponentMixin(AbstractComponent):
         :param filename: string for file name
         :return: PurePath object
         """
-        return self._dir_by_state(direction, state) / filename.strip("/ ")
+        path = self._dir_by_state(direction, state) / filename.strip("/ ")
+        if prefix:
+            path = prefix / path
+        return path
 
     def _get_remote_file(self, state, filename=None, binary=False):
         """Get file for current exchange_record in the given destination state.
@@ -62,7 +66,10 @@ class EDIStorageComponentMixin(AbstractComponent):
         :return: remote file content as string
         """
         filename = filename or self.exchange_record.exchange_filename
-        path = self._remote_file_path(self.exchange_record.direction, state, filename)
+        path_prefix = self._get_exchange_type_path()
+        path = self._remote_file_path(
+            self.exchange_record.direction, state, filename, prefix=path_prefix
+        )
         try:
             # TODO: support match via pattern (eg: filename-prefix-*)
             # otherwise is impossible to retrieve input files and acks
@@ -70,3 +77,29 @@ class EDIStorageComponentMixin(AbstractComponent):
             return self.storage.get(path.as_posix(), binary=binary)
         except FileNotFoundError:
             return None
+
+    def _get_exchange_type_path(self):
+        """Retrieve specific path for current exchange type.
+
+        In your exchange type you can pass this config:
+
+            storage:
+                # simple string
+                path: path/to/file
+
+        Or
+
+            storage:
+                # name of the param containing the path
+                path_config_param: path/to/file
+
+        Thanks to the param you could even configure it by env.
+        """
+        path = self.storage_settings.get("path")
+        if path:
+            return PurePath(path)
+        path_config_param = self.storage_settings.get("path_config_param")
+        if path_config_param:
+            icp = self.env["ir.config_parameter"].sudo()
+            path = icp.get_param(path_config_param)
+            return PurePath(path)
