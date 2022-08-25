9d452ceafc1c255165aed16cb1c1664df64f88ab
Simone Orsi <simahawk@gmail.com>
2022-08-25 18:07:12 +0200

    fixup! edi: add 'ack for' on type

--- edi_oca/models/edi_exchange_type.py
+++ edi_oca/models/edi_exchange_type.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pytz import timezone, utc
 
 from odoo import _, api, exceptions, fields, models
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT, groupby
 
 from odoo.addons.base_sparse_field.models.fields import Serialized
 from odoo.addons.http_routing.models.ir_http import slugify
@@ -63,10 +63,10 @@ class EDIExchangeType(models.Model):
         help="Identify the type of the ack. "
         "If this field is valued it means an hack is expected.",
     )
-    ack_for_type_id = fields.Many2one(
+    ack_for_type_ids = fields.Many2many(
         string="Ack for exchange type",
         comodel_name="edi.exchange.type",
-        compute="_compute_ack_for_type_id",
+        compute="_compute_ack_for_type_ids",
     )
     advanced_settings_edit = fields.Text(
         string="Advanced YAML settings",
@@ -145,11 +145,11 @@ class EDIExchangeType(models.Model):
         # This would help documenting core and custom keys.
         return yaml.safe_load(self.advanced_settings_edit or "") or {}
 
-    def _compute_ack_for_type_id(self):
+    def _compute_ack_for_type_ids(self):
         ack_for = self.search([("ack_type_id", "in", self.ids)])
-        by_type_id = {x.ack_type_id.id: x for x in ack_for}
+        by_type_id = dict(groupby(ack_for, lambda x: x.ack_type_id.id))
         for rec in self:
-            rec.ack_for_type_id = by_type_id.get(rec.id)
+            rec.ack_for_type_ids = [x.id for x in by_type_id.get(rec.id, [])]
 
     def get_settings(self):
         return self.advanced_settings
