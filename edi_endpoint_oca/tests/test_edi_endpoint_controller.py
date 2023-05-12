bd0eb11061e35dbc65efc75af4183c29101fb3ff
Simone Orsi <simahawk@gmail.com>
2023-05-12 10:50:27 +0200

    fixup! edi_endpoint: use requests for tests

--- edi_endpoint_oca/tests/test_edi_endpoint_controller.py
+++ edi_endpoint_oca/tests/test_edi_endpoint_controller.py
@@ -5,13 +5,7 @@
 import os
 import unittest
 
-import requests
-
-from odoo import tools
 from odoo.tests.common import HttpSavepointCase
-
-HOST = "127.0.0.1"
-PORT = tools.config["http_port"]
 
 
 @unittest.skipIf(os.getenv("SKIP_HTTP_CASE"), "EDIEndpointHttpCase skipped")
@@ -24,10 +18,10 @@ class EDIEndpointHttpCase(HttpSavepointCase):
 
     def test_call1(self):
         endpoint = "/edi/demo/try"
-        url = "http://{}:{}{}".format(HOST, PORT, endpoint)
-        response = requests.get(url)
+        response = self.url_open(endpoint)
         self.assertEqual(response.status_code, 401)
         # Let's login now
-        response = requests.get(url, auth=("admin", "admin"))
+        self.authenticate("admin", "admin")
+        response = self.url_open(endpoint)
         self.assertEqual(response.status_code, 200)
         self.assertIn("Created record:", response.content.decode())
