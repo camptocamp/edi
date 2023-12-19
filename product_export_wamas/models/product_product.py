6828ea6da40dc98f8915b3182fec694c19e279d6
duongtq <duongtq@trobz.com>
2023-12-19 11:10:45 +0700

    [ADD] product_export_wamas

---
+++ product_export_wamas/models/product_product.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _wamas_export(self, specific_dict=None):
+        """
+        Export the product data as WAMAS format
+
+        :return: product data as WAMAS format
+        :rtype: a string
+        """
+        self.ensure_one()
+        dict_product = self.read()[0]
+        # If having a specific dict for the product, we use it
+        if specific_dict and isinstance(specific_dict, dict):
+            dict_product = specific_dict
+        # Use "ART" telegram for exporting product
+        return self.env["base.wamas.ubl"].export_dict2wamas(dict_product, "ART")
