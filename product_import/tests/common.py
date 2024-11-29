4f485a12aeaaf31564242a515b96d6e6b4c56dd9
Florent Xicluna <florent.xicluna@camptocamp.com>
2024-11-29 09:51:10 +0100

    [IMP] product_import: Import products asynchronously

--- product_import/tests/common.py
+++ product_import/tests/common.py
@@ -11,7 +11,8 @@ class TestCommon(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.wiz_model = cls.env["product.import"]
+        # Execute directly, no job
+        cls.wiz_model = cls.env["product.import"].with_context(queue_job__no_delay=True)
         cls.supplier = cls.env["res.partner"].create({"name": "Catalogue Vendor"})
 
     def _mock(self, method_name):
