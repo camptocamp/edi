708428e1d3ad899f883f322076131b0ff9efe690
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-10-09 10:51:41 +0200

    [IMP]account_invoice_export: add details in exception post request

--- account_invoice_export/models/account_move.py
+++ account_invoice_export/models/account_move.py
@@ -108,8 +108,8 @@ class AccountMove(models.Model):
         res = requests.post(url, headers=headers, files=file_data, timeout=10)
         if res.status_code != 200:
             raise UserError(
-                self.env._("HTTP error %s sending invoice to %s")
-                % (res.status_code, self.transmit_method_id.name)
+                self.env._("HTTP error %s sending invoice to %s. %s")
+                % (res.status_code, self.transmit_method_id.name, res.text)
             )
         self.invoice_exported = self.invoice_export_confirmed = True
         return res.text
